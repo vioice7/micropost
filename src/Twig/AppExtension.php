4ee706f06b0bf474e6fcd61e24944927ea82db7e
Vio <vioice7@yahoo.com>
2021-07-28 11:38:06 +0300

    error messaging handle

--- src/Twig/AppExtension.php
+++ src/Twig/AppExtension.php
@@ -4,13 +4,27 @@ namespace App\Twig;
     
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFilter;
+use Twig\Extension\GlobalsInterface;
 
-class AppExtension extends AbstractExtension {
+class AppExtension extends AbstractExtension implements GlobalsInterface
+{
+
+    public function __construct(string $locale)
+    {
+        $this->locale = $locale;
+    }
 
     public function getFilters()
     {
         return [
             new TwigFilter('price', [$this, 'priceFilter'])
+        ];
+    }
+
+    public function getGlobals(): array
+    {
+        return [
+            'locale' => $this->locale
         ];
     }
 
