1e72bfe4c33a61c131d36af253e594d2e22a8ca3
Vio <vioice7@yahoo.com>
2021-08-09 15:55:58 +0300

    like dislike button model post

--- src/Entity/MicroPost.php
+++ src/Entity/MicroPost.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use App\Repository\MicroPostRepository;
+use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 
@@ -36,6 +37,19 @@ class MicroPost
      * @ORM\JoinColumn(nullable=false)
      */
     private $user;
+
+    /**
+     * @ORM\ManyToMany(targetEntity="App\Entity\User", inversedBy="postsLiked")
+     * @ORM\JoinTable(name="post_likes",
+     *                 joinColumns={@ORM\JoinColumn(name="post_id", referencedColumnName="id")},
+     *          inverseJoinColumns={@ORM\JoinColumn(name="user_id", referencedColumnName="id")}
+     * )
+     */
+    private $likedBy;
+
+    public function __construct() {
+        $this->likedBy = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -103,4 +117,22 @@ class MicroPost
 
         return $this;
     }
+
+    /**
+     * @return Collection
+     */ 
+    public function getLikedBy()
+    {
+        return $this->likedBy;
+    }
+
+    public function like(User $user)
+    {
+        if($this->likedBy->contains($user)) {
+            return;
+        }
+
+        $this->likedBy->add($user);
+    }
+
 }
